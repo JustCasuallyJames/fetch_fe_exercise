8e699c9704cac2fa4154f62d334ee1a9cb154b7b
James Vo <justcasuallyjames@gmail.com>
2025-02-12 03:42:54 -0800

    Preparing for deployments

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 "use client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "@/app/components/Login.js";
 
 export default function Home() {
   return (
-    <div>
-      <Login></Login>
-    </div>
+    <Router>
+      <Routes>
+        {/* Set the login page as the default route */}
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
