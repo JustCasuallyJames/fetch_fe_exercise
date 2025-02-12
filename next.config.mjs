fee9ef0c92e84a4b896c740811a54dbb550662a9
James Vo <justcasuallyjames@gmail.com>
2025-02-12 11:53:51 -0800

    deleted content within next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // For deploying on GitHub Pages in a subdirectory
-    basePath: '/fetch_fe_exercise',  // Replace 'your-repo-name' with your actual GitHub repository name
-    assetPrefix: '/fetch_fe_exercise',  // Same as above
-  
-    // Ensures all URLs have trailing slashes for better routing in static export
-    exportTrailingSlash: true,
-  
-    // Optional: If you're using any environment variables for production, make sure to set them here
-    env: {
-      CUSTOM_ENV: process.env.CUSTOM_ENV || 'default_value',
-    },
-  };
+const nextConfig = {};
   
   export default nextConfig;
